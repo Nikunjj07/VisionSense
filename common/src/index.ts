f8fd70f427cc0ac615727e95fa203627e77fab92
Nikunj <nikunjskabra@gmail.com>
2025-12-25 10:38:56 +0530

    react config

--- common/src/index.ts
+++ common/src/index.ts
@@ -2,19 +2,25 @@ import z, { email } from "zod";
 
 export const signupInput = z.object({
     name: z.string().min(2).max(15),
-    email: z.email(),
+    email: z.string().email(),
     password: z.string().min(6)
 })
 
 export type SignupType = z.infer<typeof signupInput>;
 
 export const signinInput = z.object({
-    email: z.email(),
+    email: z.string().email(),
     password: z.string().min(6)
 })
 
 export type SigninType = z.infer<typeof signinInput>
 
-export const initalizeSession = z.object({
-    id
+export const initializeSession = z.object({
+    sessionId: z.string(),
+    class: z.string(),
+    duration: z.number()
 })
+
+export type InitializeSessionType = z.infer<typeof initializeSession>
+
+export const studentData
---
+++ project.md
@@ -0,0 +1,6 @@
+This is a project for attendance montitoring for students.
+This contains the business logic of the project.
+This contains three folders client, common and server.
+Client folder contains the client side code which is in react.
+Common folder contains the common code which is a typescript file defining the schema of the data using zod.
+Server folder contains the server side code which is in nodejs + express and typescript.
